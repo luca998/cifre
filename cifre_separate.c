25fc3926ca809a28142e4e5d8ed63fa1ffa17759
luca998 <luca.caracciolo16@gmail.com>
2016-11-07 19:21:24 +0100

    cifre_separate.c

---
+++ cifre_separate.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+  int num;
+  
+
+  printf("Inserisci un numero di cinque cifre: ");
+  scanf("%d", &num);
+
+  if (num > 9999 && num < 99999) {
+    printf("%d\n", num);
+   }
+  else
+  printf("Errore\n");
+
+  return 0;}
