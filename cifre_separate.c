ee353dd74e06097658e4eb26358306766d1b6792
luca998 <luca.caracciolo16@gmail.com>
2016-11-08 21:26:05 +0100

    cifre_separate.c

--- cifre_separate.c
+++ cifre_separate.c
@@ -3,7 +3,8 @@
 int main() 
 {
   int num;
-  int cifra;
+  int cifra5,cifra4,cifra3,cifra2,cifra1;
+  int d1,d2,d3,d4;
   
 
   printf("Inserisci un numero di cinque cifre: ");
@@ -13,16 +14,16 @@ int main()
     return -1;
   }
 
-    cifra= (num / 10000) %10;
-    printf("%d   ",cifra);
-    cifra= (num / 1000) %10;
-    printf("%d   ",cifra);
-    cifra= (num / 100) %10;
-    printf("%d   ",cifra);
-    cifra= (num / 10) %10;
-    printf("%d   ",cifra);
-     cifra= num % 10;
-    printf("%d   ",cifra);
+    cifra5= num %10;
+    d1= (num / 10);
+    cifra4= d1 % 10;
+    d2= (d1 / 10);
+    cifra3= d2 % 10;
+    d3= (d2 / 10);
+    cifra2= d3 % 10;
+    d4= (d3 / 10);
+    cifra1= d4 % 10;
+    printf("%d   %d   %d   %d   %d",cifra1,cifra2,cifra3,cifra4,cifra5);
 
   printf("\n");
 
