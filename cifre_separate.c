64535b2da913ebe71f80f7652aa0139a6734943a
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-11-08 14:17:21 +0100

    aggiustamenti

--- cifre_separate.c
+++ cifre_separate.c
@@ -3,17 +3,22 @@
 int main() 
 {
   int num;
-  int est; 
-  printf("Inserisci un numero di cinque cifre: ");
-   scanf("%d", &num);
+  int est;  /* che significa est? trova un nome migliore */
 
-  if (num<10000 || num>99999) 
-    {
-    printf("Errore\n");
-     }
-  else { 
-     printf("Il numero e':%d\n",num);
-     est=num%10;
-     printf("L'ultima cifra e':%d\n",est);
-       } 
-  return 0;}
+  printf("Inserisci un numero di cinque cifre: ");
+  scanf("%d", &num);
+
+  /* caso negativo, se il numero non va bene esci dal programma */
+  if (num < 10000 || num > 99999) {
+    return -1;
+  }
+
+  /* else non necessario, se siamo qui significa che num
+   * e' proprio di cinque cifre
+   */
+  printf("Il numero e':%d\n",num);
+  est=num%10;
+  printf("L'ultima cifra e':%d\n",est);
+
+  return 0;
+}
